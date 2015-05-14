7902372a9d365ea820b465985664ee26d4f25923
vhsharom <vhsharo@gmail.com>
2015-05-14 17:21:07 -0500

    Commit Victor

--- Repositorios2/ViewController.swift
+++ Repositorios2/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("Hello World")
+        println("Victor")
     }
 
     override func didReceiveMemoryWarning() {
